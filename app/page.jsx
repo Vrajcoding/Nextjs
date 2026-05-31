ddf0099387c48144e4b8ae1b8a3b1df102df9990
Vrajcoding <vraj11@gmail.com>
2026-05-31 18:50:23 +0530

    fixed all the files for mahi ganesh

--- app/page.jsx
+++ app/page.jsx
@@ -2,11 +2,7 @@ function main() {
     return ( 
         <div>
             <div>
-<<<<<<< HEAD
                 Hello MG 
-=======
-                Hello this is my branch
->>>>>>> 986e53b0c2920ae1ca73c35d2c9ff74adea8da3d
             </div>
         </div>
      );
